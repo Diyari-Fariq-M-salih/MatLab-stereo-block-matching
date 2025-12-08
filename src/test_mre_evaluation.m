b27e7774f9379708be417838e15c48f0bf839630
Diyari-Fariq-M-salih <diyari.m.salih@gmail.com>
2025-12-08 11:07:07 +0100

    evaluation plots

--- src/test_mre_evaluation.m
+++ src/test_mre_evaluation.m
@@ -13,3 +13,20 @@ DL_filled = fill_holes(DL);
 mre_value = mre_evaluation(DL_filled, D_gt);
 
 fprintf("MRE (Mean Relative Error): %.4f\n", mre_value);
+absError = abs(DL_filled - D_gt);
+absError(isnan(absError)) = 0;
+
+figure;
+subplot(1,3,1);
+imagesc(DL_filled); colormap jet; colorbar;
+title("Estimated disparity");
+
+subplot(1,3,2);
+imagesc(D_gt); colormap jet; colorbar;
+title("Ground truth disparity");
+
+subplot(1,3,3);
+imagesc(absError); colormap hot; colorbar;
+title("Absolute error map");
+
+sgtitle("Evaluation of Disparity Map");
